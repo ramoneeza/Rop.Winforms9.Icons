bed71d17389c1e2989fbad8310d8940dbce437ec
Ramon <ramon@eeza.csic.es>
2024-11-15 09:42:38 +0100

    Nueva version

---
+++ Rop.Winforms9.DuotoneIcons/publishall.cmd
@@ -0,0 +1,16 @@
+echo #### DoutoneIcons
+nugetpublish -bklocal \\eeza.csic.es\nugeteeza$
+echo #### ColumnsListBox
+cd ..\Rop.Winforms9.ColumnsListBox
+call .\publish.cmd
+echo #### DuotoneIcons.MaterialDesign
+cd ..\Rop.Winforms9.DuotoneIcons.MaterialDesign
+call .\publish.cmd
+echo #### DuotoneIcons.GoogleMaterial
+cd ..\Rop.Winforms9.DuotoneIcons.GoogleMaterial
+call .\publish.cmd
+echo #### FIN
+cd ..\Rop.Winforms9.DuotoneIcons
+
+
+
